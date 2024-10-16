f9e262badfdbbdcdcf7847d58790c22c12494b22
yaoxunxu <yaoxunxu@tencent.com>
2024-10-16 20:44:12 +0800

    update mix

--- tools/torch_tools.py
+++ tools/torch_tools.py
@@ -3,7 +3,7 @@ import torchaudio
 import random
 import itertools
 import numpy as np
-from tools.mix import mix
+
 
 
 def normalize_wav(waveform):
@@ -98,46 +98,3 @@ def uncapitalize(s):
     else:
         return ""
 
-    
-def mix_wavs_and_captions(path1, path2, caption1, caption2, target_length=1024):
-    sound1 = read_wav_file(path1, target_length * 160)[0].numpy()
-    sound2 = read_wav_file(path2, target_length * 160)[0].numpy()
-    mixed_sound = mix(sound1, sound2, 0.5, 16000).reshape(1, -1)
-    mixed_caption = "{} and {}".format(caption1, uncapitalize(caption2))
-    return mixed_sound, mixed_caption
-
-
-def augment(paths, texts, num_items=4, target_length=1024):
-    mixed_sounds, mixed_captions = [], []
-    combinations = list(itertools.combinations(list(range(len(texts))), 2))
-    random.shuffle(combinations)
-    if len(combinations) < num_items:
-        selected_combinations = combinations
-    else:
-        selected_combinations = combinations[:num_items]
-        
-    for (i, j) in selected_combinations:
-        new_sound, new_caption = mix_wavs_and_captions(paths[i], paths[j], texts[i], texts[j], target_length)
-        mixed_sounds.append(new_sound)
-        mixed_captions.append(new_caption)
-        
-    waveform = torch.tensor(np.concatenate(mixed_sounds, 0))
-    waveform = waveform / torch.max(torch.abs(waveform))
-    waveform = 0.5 * waveform
-    
-    return waveform, mixed_captions
-
-
-def augment_wav_to_fbank(paths, texts, num_items=4, target_length=1024, fn_STFT=None):
-    assert fn_STFT is not None
-    
-    waveform, captions = augment(paths, texts)
-    fbank, log_magnitudes_stft, energy = get_mel_from_wav(waveform, fn_STFT)
-    fbank = fbank.transpose(1, 2)
-    log_magnitudes_stft = log_magnitudes_stft.transpose(1, 2)
-
-    fbank, log_magnitudes_stft = _pad_spec(fbank, target_length), _pad_spec(
-        log_magnitudes_stft, target_length
-    )
-
-    return fbank, log_magnitudes_stft, waveform, captions
